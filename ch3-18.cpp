bef115f6381ad113e28f6b80d8d6fcc0a380a671
Brenna Meyer <brennameyer9@gmail.com>
2025-02-12 14:15:02 -0500

    Add project files.

---
+++ ch3-18.cpp
@@ -0,0 +1,29 @@
+﻿// ch3-18.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Interest Earned
+//Assuming there are no deposits other than the original investment, the balance in a savings account after one year may be calculated as
+//Amount = Principal × (1 + RateT)T
+//Write a program that asks for the principal, the interest rate, and the number of times the interest is compounded.It should display a report similar to :
+
+//Interest Rate : 4.25 %
+//Times Compounded : 12
+//Principal : $ 1000.00
+//Interest : $   43.34
+//Amount in Savings : $ 1043.34
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
