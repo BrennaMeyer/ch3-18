5e45ca02c4b63e0fe5a012e207ffdcedaa0f35b1
Brenna Meyer <brennameyer9@gmail.com>
2025-02-13 19:21:11 -0500

    Code Finished

--- ch3-18.cpp
+++ ch3-18.cpp
@@ -1,4 +1,6 @@
 ﻿// ch3-18.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Programmer: Brenna Meyer
+// Date:February 13th,2025
 //Interest Earned
 //Assuming there are no deposits other than the original investment, the balance in a savings account after one year may be calculated as
 //Amount = Principal × (1 + RateT)T
@@ -8,22 +10,66 @@
 //Times Compounded : 12
 //Principal : $ 1000.00
 //Interest : $   43.34
-//Amount in Savings : $ 1043.34
+//Amount in Savings : $ 1043.34;
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int one = 1,
+		oneHundred = 100;
+
+	float principal,
+		totalIntrest,
+		amountAfterAYear,
+		intrestRate,
+		amountInSavings;
+
+	int timesIntrestCompounded;
+
+		cout << endl;
+		cout << "What's the principle balance in the savings?";
+	cin>> principal;
+
+	cout << "What's the intrest rate? %";
+	cin >> intrestRate;
+
+	cout << "How many times is intrest\n";
+	cout << "compounded per year?";
+	cin >> timesIntrestCompounded;
+	cout << endl;
+
+	//Math;
+	intrestRate /= oneHundred;
+
+	amountAfterAYear = principal * ((one + intrestRate / timesIntrestCompounded), timesIntrestCompounded);
+
+	totalIntrest = amountAfterAYear - principal;
+
+	intrestRate *= oneHundred;
+
+
+		//display;
+
+	cout << "Intrest Rate: ";
+	cout << intrestRate << "%" << endl;
+
+	cout << "Times Compounded: ";
+	cout << timesIntrestCompounded << endl;
+
+	cout << "Principal $: ";
+	cout << principal << endl;
+
+	cout << "intrest: $";
+	cout << intrestRate << endl;
+
+	cout << "Amount after a year: $";
+	cout << amountAfterAYear << endl;
+	
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
